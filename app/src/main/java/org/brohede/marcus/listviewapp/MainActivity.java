1d73cfb17c035fb38a64bf4ffc94fd4015e2818a
Jacob Svensson <jacobsvenssonn@gmail.com>
2018-04-03 10:31:08 +0200

    Löst klart uppgiften.

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -30,16 +30,15 @@ public class MainActivity extends AppCompatActivity {
     private int[] mountainHeights ={4478,4808,6190};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // 1. Create an array
-        String[] rawData = {"Jacob", "Olle", "Adam", "Rasmus"};
+        //String[] rawData = {"Jacob", "Olle", "Adam", "Rasmus"};
         // 2. Create a List object with your array from step 1 as in-data
-        List<String> listData = new ArrayList<String>(Arrays.asList(rawData));
+        final List<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
         // 3. Create an ArrayAdapter object that connects
         final ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview, R.id.my_item_textview, listData);
 
@@ -47,13 +46,11 @@ public class MainActivity extends AppCompatActivity {
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "Tjena "+ adapter.getItem(position).toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), mountainLocations[position] +  ", " +  Integer.toString(mountainHeights[position]), Toast.LENGTH_SHORT).show();
             }
         });
 
         myListView.setAdapter(adapter);
-
-        adapter.add("Rolf");
     }
 
     @Override
